af776b793ea3eb94aa657dcfe98682304a3e4fea
Mathieu Agopian <mathieu@agopian.info>
2016-10-29 14:10:57 +0200

    Add rain.py for a simple demo. main.py is now a loader

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 from microbit import *
-import mood
+
+
+# Seems "importlib.import_module" doesn't exist on micropython. Thus this hack.
+def mood():
+    import mood
+
+
+def rain():
+    import rain
+
+
+CHOICES = {
+    "Mood": mood,
+    "Rain": rain,
+}
+
+while True:
+    if button_a.was_pressed():
+        CHOICES["Mood"]()
+    elif button_b.was_pressed():
+        CHOICES["Rain"]()

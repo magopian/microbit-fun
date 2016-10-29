82635ddd61f8dd9b45907ab25be132bc44e67cc6
Mathieu Agopian <mathieu@agopian.info>
2016-10-29 18:02:31 +0200

    Improve the main.py loader

--- main.py
+++ main.py
@@ -15,8 +15,18 @@ CHOICES = {
     "Rain": rain,
 }
 
-while True:
-    if button_a.was_pressed():
-        CHOICES["Mood"]()
-    elif button_b.was_pressed():
-        CHOICES["Rain"]()
+while True:  # Outer loop: keep cycling between choices.
+    for choice, import_function in CHOICES.items():
+        display.scroll(choice, wait=False, loop=True)
+        # Inner loop: keep scrolling current choice.
+        a_pressed = button_a.was_pressed()
+        b_pressed = button_b.was_pressed()
+        while not a_pressed and not b_pressed:
+            # sleep(100)
+            a_pressed = button_a.was_pressed()
+            b_pressed = button_b.was_pressed()
+
+        if a_pressed:  # Skip this demo, display the next choice.
+            continue
+        elif b_pressed:  # Choose this demo, run it.
+            import_function()

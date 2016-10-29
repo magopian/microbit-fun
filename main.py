2593a8655d574c022c9763cad970aeebeac4f4e2
Mathieu Agopian <mathieu@agopian.info>
2016-10-29 19:35:49 +0200

    Improve the main.py loader a bit more (and simplify it)

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
+import os
 from microbit import *
 
 
-# Seems "importlib.import_module" doesn't exist on micropython. Thus this hack.
-def mood():
-    import mood
+files = os.listdir()
+modules = [file_[:-3] for file_ in files
+           if file_ != 'main.py' and file_.endswith('.py')]
 
 
-def rain():
-    import rain
-
-
-CHOICES = {
-    "Mood": mood,
-    "Rain": rain,
-}
-
-while True:  # Outer loop: keep cycling between choices.
-    for choice, import_function in CHOICES.items():
-        display.scroll(choice, wait=False, loop=True)
-        # Inner loop: keep scrolling current choice.
+while True:  # Outer loop: cycle between choices.
+    for module in modules:
+        display.scroll(module, wait=False, loop=True)
+        # Inner loop: keep scrolling current choice until a button is pressed.
         a_pressed = button_a.was_pressed()
         b_pressed = button_b.was_pressed()
         while not a_pressed and not b_pressed:
-            # sleep(100)
             a_pressed = button_a.was_pressed()
             b_pressed = button_b.was_pressed()
 
-        if a_pressed:  # Skip this demo, display the next choice.
+        if a_pressed:  # Skip this one, display the next choice.
             continue
-        elif b_pressed:  # Choose this demo, run it.
-            import_function()
+        elif b_pressed:  # Run the current choice.
+            __import__(module)

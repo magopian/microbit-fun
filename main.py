e4c7a31078a1fdd6ba0f81dd1b8caa12b70a0b17
Mathieu Agopian <mathieu@agopian.info>
2016-10-30 19:05:25 +0100

    Improve the main.py loader: help message if no module, run module if only one present

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ This will only work if you have flashed an "empty micropython runtime" hex file
 The loader will display the first module that it found on the micro:bit
 filesystem. To skip to the next one, use the A button. To run the current
 module, press B.
+
+If there's only one module present, it'll run it straight away, without having
+to select it.
 """
 import os
 from microbit import *
@@ -18,6 +21,13 @@ modules = [file_[:-3] for file_ in files
 
 
 while True:  # Outer loop: cycle between choices.
+    if not modules:
+        display.scroll(
+            "Upload micropython modules to the micro:bit filesystem using the "
+            "ufs utility",
+            loop=True)
+    if len(modules) == 1:  # Only one module? Run it straight away.
+        __import__(modules[0])
     for module in sorted(modules):
         display.scroll(module, wait=False, loop=True)
         # Inner loop: keep scrolling current choice until a button is pressed.
